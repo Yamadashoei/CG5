ab683269bac51a0afe674646f3fbbf06c20caa68
Yamadashoei <k023g0053@g.neec.ac.jp>
2025-04-16 12:13:39 +0900

    青ウィンドウ描画完了

--- main.cpp
+++ main.cpp
@@ -1,22 +1,36 @@
-#include <Windows.h>
 #include "KamataEngine.h"
+#include <Windows.h>
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	
+
+	// 初期化
+	KamataEngine::Initialize(L"LE3C_27_ヤマダ_ショウエイ");
+	// DirectXCommonのインスタンスを取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// 描画開始 ↓ 描画記述
+		dxCommon->PreDraw();
 
 
 
 
+		// 描画終了
+		dxCommon->PostDraw();
+	}
 
-
-
-
-
-
-
-
-
+	// 解放処理
+	// エンジンの終了処理
+	KamataEngine::Finalize();
 
 	return 0;
 }
