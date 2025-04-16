149926339704745a1060cc3f5659b98ff817413f
Yamadashoei <k023g0053@g.neec.ac.jp>
2025-04-16 12:01:54 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <Windows.h>
+#include "KamataEngine.h"
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
