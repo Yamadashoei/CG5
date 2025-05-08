59a1ae2990da8212bc8171fd6deee856cd5f4b03
Yamadashoei <k023g0053@g.neec.ac.jp>
2025-05-08 10:11:54 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace KamataEngine;
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// エンジンの初期化
-	KamataEngine::Initialize(L"LE3D_07_ササノ_ミカゼ_CG4");
+	KamataEngine::Initialize(L"LE3C_27_ヤマダ_ショウエイ");
 
 	// DirectXCommonインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
