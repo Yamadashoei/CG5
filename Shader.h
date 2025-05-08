460d72985582f206dd1dbb5a116c78f0f656f81a
Yamadashoei <k023g0053@g.neec.ac.jp>
2025-05-08 10:55:06 +0900

    完了

---
+++ Shader.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <d3d12.h>
+#include <string>
+
+class Shader {
+public:
+	// シェーダーファイルを読み込み、コンパイル済みデータを生成する
+	void Load(const std::wstring& filePath, const std::string& ShaderModel);
+
+	// 生成したコンパイル済みデータを取得する
+	ID3DBlob* GetBlob();
+
+	// コンストラクタ
+	Shader();
+	// デストラクタ
+	~Shader();
+
+private:
+	ID3DBlob* blob_ = nullptr;
+};
