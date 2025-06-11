c75a53d30394ed4bbbdecf385abff0977cb3e7f8
Yamadashoei <k023g0053@g.neec.ac.jp>
2025-06-11 11:31:57 +0900

    完了

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -3,6 +3,7 @@
 #include "GameScene.h"
 #include <Windows.h>
 
+#include "IndexBuffer.h"
 #include "PipelineState.h"
 #include "RootSignature.h"
 #include "Shader.h"
@@ -62,20 +63,54 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 #pragma endregion
 
+#pragma region VertxDataを作成
+	struct VertexData {
+		Vector4 position;
+	};
+
+	// 頂点データ準備
+	VertexData vertices[] = {
+	    {-1.0f, 1.0f,  0.0f, 1.0f}, // 左上
+	    {1.0f,  1.0f,  0.0f, 1.0f}, // 右上
+	    {1.0f,  -1.0f, 0.0f, 1.0f}, // 右下
+	    {-1.0f, -1.0f, 0.0f, 1.0f}, // 左下
+	};
+
+#pragma endregion
+
 #pragma region VertxBuffer
 	VertexBuffer vb;
-	vb.Create(sizeof(Vector4) * 3, sizeof(Vector4));
+	vb.Create(sizeof(vertices), sizeof(vertices[0])); // 00_07で変更
+
+	// 頂点リソースにデータを書き込む
+	VertexData* pGpuVertices = nullptr;
+	vb.Get()->Map(0, nullptr, reinterpret_cast<void**>(&pGpuVertices));
+
+	for (int i = 0; i < _countof(vertices); ++i) {
+		pGpuVertices[i] = vertices[i];
+	}
 
 #pragma endregion
 
 #pragma region 頂点リソースにデータを書き込む
-	Vector4* vertexData = nullptr;
-	vb.Get()->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
-	vertexData[0] = {-0.5f, -0.5f, 0.0f, 1.0f}; // 左下
-	vertexData[1] = {0.0f, 0.5f, 0.0f, 1.0f};   // 上
-	vertexData[2] = {0.5f, -0.5f, 0.0f, 1.0f};  // 右下
-	                                            // 頂点リソースのマップを解除する
-	                                            // vb.Get()->Unmap(0, nullptr);
+	uint16_t indices[] = {
+	    0, 1, 2, // 左上 右上 右下
+	    2, 3, 0, // 右下 左下 左上
+	};
+#pragma endregion
+
+#pragma region IndexBuffer
+	IndexBuffer ib;
+	ib.Create(sizeof(indices), sizeof(indices[0]));
+
+	// 頂点リソースにデータを書き込む
+	uint16_t* pGpuIndices = nullptr;
+	ib.Get()->Map(0, nullptr, reinterpret_cast<void**>(&pGpuIndices));
+
+	for (int i = 0; i < _countof(indices); ++i) {
+		pGpuIndices[i] = indices[i];
+	}
+
 #pragma endregion
 
 	// メインループ
@@ -97,10 +132,11 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		commandList->SetGraphicsRootSignature(rs.Get());     // rootSignatureの設定
 		commandList->SetPipelineState(pipelineState.Get());  // PSOの設定をする
 		commandList->IASetVertexBuffers(0, 1, vb.GetView()); // VBVの設定
+		commandList->IASetIndexBuffer(ib.GetView());         // IBVの設定
 		// トロポジの設定
 		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 		// 頂点数、インデックス数、インデックスの開始位置、インデックスのオフセット
-		commandList->DrawInstanced(3, 1, 0, 0);
+		commandList->DrawIndexedInstanced(_countof(indices), 1, 0, 0, 0);// commandList->DrawInstanced(3, 1, 0, 0);
 
 		// 描画終了
 		dxCommon->PostDraw();
