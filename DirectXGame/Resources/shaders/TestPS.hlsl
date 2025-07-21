c7eda10a0e4e2fbd64c1346a1c6488c9cccd0668
Yamadashoei <k023g0053@g.neec.ac.jp>
2025-07-21 16:30:44 +0900

    完成

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -18,11 +18,11 @@ PixelShaderOutput main(VertexShaderOutput input)
     float32_t value = dot(textureColor.rgb, float32_t3(0.2125f, 0.7154f, 0.0721f));
     float32_t4 color = float32_t4(value, value, value, textureColor.a); 
 
-    // Vignette処理（加点要素②）
-    float2 center = float2(0.5f, 0.5f);
-    float dist = distance(uv, center);
-    float vignette = smoothstep(0.8f, 0.3f, dist);
-    color.rgb *= vignette;
+    // Vignette処理
+    float2 center = float2(0.5f, 0.5f); // 画面の中心に設定
+    float dist = distance(uv, center); // UV座標と中心の距離を計算
+    float vignette = smoothstep(0.6f, 0.3f, dist); // vignette値を計算
+    color.rgb *= vignette; // vignette
 
     output.color = color;
     return output;
