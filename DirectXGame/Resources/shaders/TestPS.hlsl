13b9bfb00e82c92d871a545a002890a5734378fd
Yamadashoei <k023g0053@g.neec.ac.jp>
2025-07-23 11:35:59 +0900

    完成

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -5,7 +5,7 @@ SamplerState gSampler : register(s0);
 
 struct PixelShaderOutput
 {
-    float32_t4 color : SV_TARGET0;
+    float32_t4 color : SV_TARGET0; 
 };
 
 PixelShaderOutput main(VertexShaderOutput input)
